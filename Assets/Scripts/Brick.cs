b09140e8e74a29c87f313f8b9ee6a89f587b97c3
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-03 07:57:22 -0800

    Added brick rotation animation & restructured brick layout

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     [SerializeField] private int numHits = 1;
-    [SerializeField] private int points;
+    // [SerializeField] private int points;
+    [SerializeField] private Vector3 rotator;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        // numHits = 1;
-        points  = 100;
+        transform.Rotate(rotator * (transform.position.x + transform.position.y) * 0.1f);
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(rotator * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision other) {
