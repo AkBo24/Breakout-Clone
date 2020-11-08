11974a3fc63216c9f1926ae64d1aaeacff514000
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-07 17:31:24 -0800

    Basic game states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,12 +14,69 @@ public class GameManager : MonoBehaviour {
     //UI Panels
     private GameObject panelMenue, panelPlay, panelLevelCompleted, panelGameOver;
 
+    private enum State { MENU, INIT, PLAY, LEVEL_COMPLETED, LOAD_LEVEL, GAMEOVER }
+    [SerializeField] private State gameState;
+
     void Start() {
         
     }
 
     // Update is called once per frame
     void Update() { 
-        
+        switch (newState) {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVEL_COMPLETED:
+                break;
+            case State.LOAD_LEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
     }
+
+    void SwitchState(State nextState) {
+        EndState();
+        BeginState(nextState);
+    }
+
+    void BeginState(State newState) {
+        switch (newState) {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVEL_COMPLETED:
+                break;
+            case State.LOAD_LEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+    void EndState() {
+        switch (gameState) {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVEL_COMPLETED:
+                break;
+            case State.LOAD_LEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+
 }
