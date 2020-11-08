a27d0d154027d36a1d1b079a255cfa46755d3e07
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-07 16:51:51 -0800

    Removed Ball & player paddle for later instantiation, set up basic text ui and play button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class GameManager : MonoBehaviour {
+
+    public GameObject ballPrefab, playerPrefab;
     void Start()
     {
         
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         
     }
 }
