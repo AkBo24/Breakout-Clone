f3cfb4b9dc0deed9f22ba405c34d9f669fd4de80
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-07 17:19:31 -0800

    Basic UI Set up, game over, level completed, panel menu, planel play

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+    //GameObjects
     public GameObject ballPrefab, playerPrefab;
-    void Start()
-    {
+
+    //UI Text
+    private Text scoreText, levelText, ballsText;
+
+    //UI Panels
+    private GameObject panelMenue, panelPlay, panelLevelCompleted, panelGameOver;
+
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    { 
+    void Update() { 
         
     }
 }
