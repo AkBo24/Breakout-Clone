f0a0524f94826b3b70e733e45a40770f2e2737a0
Akshay Bodla <akshaybodla21@gmail.com>
2020-11-07 18:54:14 -0800

    Play button initializes the PanelPlay button (nothing happens yet inside the game)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,32 +12,16 @@ public class GameManager : MonoBehaviour {
     private Text scoreText, levelText, ballsText;
 
     //UI Panels
-    private GameObject panelMenue, panelPlay, panelLevelCompleted, panelGameOver;
+    [SerializeField] GameObject panelMenu, panelPlay, panelLevelCompleted, panelGameOver;
 
     private enum State { MENU, INIT, PLAY, LEVEL_COMPLETED, LOAD_LEVEL, GAMEOVER }
     [SerializeField] private State gameState;
 
     void Start() {
-        
+        gameState = State.MENU;
+        SwitchState(gameState);
     }
 
-    // Update is called once per frame
-    void Update() { 
-        switch (newState) {
-            case State.MENU:
-                break;
-            case State.INIT:
-                break;
-            case State.PLAY:
-                break;
-            case State.LEVEL_COMPLETED:
-                break;
-            case State.LOAD_LEVEL:
-                break;
-            case State.GAMEOVER:
-                break;
-        }
-    }
 
     void SwitchState(State nextState) {
         EndState();
@@ -47,21 +31,51 @@ public class GameManager : MonoBehaviour {
     void BeginState(State newState) {
         switch (newState) {
             case State.MENU:
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                panelPlay.SetActive(true);
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVEL_COMPLETED:
+                panelLevelCompleted.SetActive(true);
+                break;
+            case State.LOAD_LEVEL:
+                break;
+            case State.GAMEOVER:
+                panelGameOver.SetActive(true);
+                break;
+        }
+    }
+
+    void EndState() {
+        switch (gameState) {
+            case State.MENU:
+                panelMenu.SetActive(false);
                 break;
             case State.INIT:
                 break;
             case State.PLAY:
                 break;
             case State.LEVEL_COMPLETED:
+                panelLevelCompleted.SetActive(false);
                 break;
             case State.LOAD_LEVEL:
                 break;
             case State.GAMEOVER:
+                panelPlay.SetActive(false);
+                panelGameOver.SetActive(false);
                 break;
         }
     }
 
-    void EndState() {
+    public void playClicked () {
+        SwitchState(State.INIT);
+    }
+
+    // Update is called once per frame
+    void Update() { 
         switch (gameState) {
             case State.MENU:
                 break;
